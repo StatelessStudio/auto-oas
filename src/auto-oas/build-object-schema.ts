7ed6d84dde05d49acde0083598a6b206789ae174
Drew Immerman <drewimmerman@gmail.com>
2025-11-18 06:34:25 -0500

    feat: add support for array schemas

--- src/auto-oas/build-object-schema.ts
+++ src/auto-oas/build-object-schema.ts
@@ -1,9 +1,23 @@
-import { ObjectSanitizer, RunsLikeAValSan } from 'valsan';
+import {
+	ArrayValSan,
+	ObjectSanitizer,
+	ObjectValSan,
+	RunsLikeAValSan,
+} from 'valsan';
 import { SchemaObject } from '../oas/v3.1';
 import { buildPropSchema } from './build-prop-schema';
 
-export function buildObjectSchema(
-	sanitizer: ObjectSanitizer
+export function buildArrayValSanSchema(
+	sanitizer: ArrayValSan
+): SchemaObject | undefined {
+	const schema: SchemaObject = { type: 'array' };
+	schema.items = buildPropSchema(sanitizer.schema as RunsLikeAValSan);
+
+	return schema;
+}
+
+export function buildObjectValSanSchema(
+	sanitizer: ObjectSanitizer | ObjectValSan
 ): SchemaObject | undefined {
 	const schema: SchemaObject = { type: 'object', properties: {} };
 
@@ -13,4 +27,17 @@ export function buildObjectSchema(
 	}
 
 	return schema;
+}
+
+export function buildObjectSchema(
+	sanitizer: ObjectSanitizer | ObjectValSan | ArrayValSan
+): SchemaObject | undefined {
+	if ('type' in sanitizer && sanitizer.type === 'array') {
+		return buildArrayValSanSchema(sanitizer as ArrayValSan);
+	}
+	else {
+		return buildObjectValSanSchema(
+			sanitizer as ObjectSanitizer | ObjectValSan
+		);
+	}
 }
